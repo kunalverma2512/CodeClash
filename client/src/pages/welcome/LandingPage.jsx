3ffae85478eae684652056fb411be5bd7857e7e2
kunalverma2512 <kunalvermah8@gmail.com>
2025-06-20 00:19:45 +0530

    fixed issue in frontend regarding Hero component

--- client/src/pages/welcome/LandingPage.jsx
+++ client/src/pages/welcome/LandingPage.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from '../../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import AboutSection1 from '../sections/AboutSection1'
 import AboutSection2 from '../sections/AboutSection2'
+import Hero from '../sections/Hero'
 
 const LandingPage = () => {
 
